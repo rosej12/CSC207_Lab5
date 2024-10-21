dfc284f5ef8dec389bde612f22fe8bd7bc45db9f
jiangros <rfh.jiang@mail.utoronto.ca>
2024-10-21 18:35:46 -0400

    Save username as instance var

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -4,9 +4,15 @@ package use_case.logout;
  * The Input Data for the Logout Use Case.
  */
 public class LogoutInputData {
+    private String username;
 
     public LogoutInputData(String username) {
-        // TODO: save the current username in an instance variable and add a getter.
+        // --TODO: save the current username in an instance variable and add a getter.
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
 }
